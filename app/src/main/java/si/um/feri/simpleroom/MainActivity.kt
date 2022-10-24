1b8bd9886c6a0dfc1d6d43dfd97f08f27562527b
HlisTilen <tilen.hlis@gmail.com>
2022-10-24 11:03:38 +0200

    update: gradle, kotlin

--- app/src/main/java/si/um/feri/simpleroom/MainActivity.kt
+++ app/src/main/java/si/um/feri/simpleroom/MainActivity.kt
@@ -6,14 +6,17 @@ import android.util.Log
 import android.widget.Toast
 import androidx.room.Room
 import kotlinx.coroutines.*
+import si.um.feri.simpleroom.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         coroutineScope.launch {
             AppDatabase.getDatabase(this@MainActivity).userDao()
